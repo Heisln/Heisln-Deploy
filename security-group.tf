a06cbbf99932bb944f00687b3062e741b5036f7e
Johannes Schumeth <johannes.schumeth@gmail.com>
2021-05-10 19:50:53 +0200

    add missing security groups

--- security-group.tf
+++ security-group.tf
@@ -30,6 +30,24 @@ resource "exoscale_security_group_rule" "db" {
   end_port = 3306
 }
 
+resource "exoscale_security_group_rule" "user-service" {
+  security_group_id = exoscale_security_group.car-rental.id
+  type = "INGRESS"
+  protocol = "TCP"
+  cidr = "0.0.0.0/0"
+  start_port = 9001
+  end_port = 9001
+}
+
+resource "exoscale_security_group_rule" "carrental-service" {
+  security_group_id = exoscale_security_group.car-rental.id
+  type = "INGRESS"
+  protocol = "TCP"
+  cidr = "0.0.0.0/0"
+  start_port = 9002
+  end_port = 9002
+}
+
 resource "exoscale_security_group_rule" "currency-converter" {
   security_group_id = exoscale_security_group.car-rental.id
   type = "INGRESS"
@@ -37,6 +55,33 @@ resource "exoscale_security_group_rule" "currency-converter" {
   cidr = "0.0.0.0/0"
   start_port = 9000
   end_port = 9000
+}
+
+resource "exoscale_security_group_rule" "rabbitmq-1" {
+  security_group_id = exoscale_security_group.car-rental.id
+  type = "INGRESS"
+  protocol = "TCP"
+  cidr = "0.0.0.0/0"
+  start_port = 15672
+  end_port = 15672
+}
+
+resource "exoscale_security_group_rule" "rabbitmq-2" {
+  security_group_id = exoscale_security_group.car-rental.id
+  type = "INGRESS"
+  protocol = "TCP"
+  cidr = "0.0.0.0/0"
+  start_port = 5672
+  end_port = 5672
+}
+
+resource "exoscale_security_group_rule" "mongo" {
+  security_group_id = exoscale_security_group.car-rental.id
+  type = "INGRESS"
+  protocol = "TCP"
+  cidr = "0.0.0.0/0"
+  start_port = 27017
+  end_port = 27017
 }
 
 resource "exoscale_security_group_rule" "ssh" {
